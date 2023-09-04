dee6431fb6fada279be3728cdea3242f746a8fe9
Pedro Gobira Pereira <64466694+pedrogobira@users.noreply.github.com>
2023-09-04 16:55:08 -0300

    feat: add spheres of power
    Add spheres of power (Municipal, Estadual e Federal/Uniao)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AreaSeeder::class);
         $this->call(SubareaSeeder::class);
         $this->call(TitleSeeder::class);
+        $this->call(SphereSeeder::class);
     }
 }
