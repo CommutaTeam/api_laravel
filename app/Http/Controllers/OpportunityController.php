a0e10b94129a0c050e1dd2b073554792a02a26fa
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-10-15 12:32:04 -0300

    fix: retorno dos dados das opotunidades

--- app/Http/Controllers/OpportunityController.php
+++ app/Http/Controllers/OpportunityController.php
@@ -14,8 +14,20 @@ class OpportunityController extends Controller
     public function index()
     {
         $user_id = auth()->id();
+
         $opportunities = Opportunity::where('user_id', $user_id)
-            ->orderBy('created_at', 'desc')
+            ->join('users', 'opportunities.interesting_user_id', '=', 'users.id')
+            ->join('cities', 'users.city_id', '=', 'cities.id')
+            ->join('subareas', 'users.subarea_id', '=', 'subareas.id')
+            ->join('titles', 'users.title_id', '=', 'titles.id')
+            ->join('organizations', 'users.organization_id', '=', 'organizations.id')
+            ->select('opportunities.id as opportunity_id', 
+                'users.first_name as user_name', 
+                'cities.title as city_name',
+                'subareas.name as subarea_name',
+                'titles.name as title_name',
+                'organizations.name as organization_name')
+            ->orderBy('opportunities.created_at', 'desc')
             ->get();
 
         return response()->json($opportunities);
@@ -26,12 +38,25 @@ class OpportunityController extends Controller
      */
     public function show(string $id)
     {
-        $contact = Opportunity::find($id);
+        $opportunity = Opportunity::where('opportunities.id', $id)
+            ->join('users', 'opportunities.interesting_user_id', '=', 'users.id')
+            ->join('cities', 'users.city_id', '=', 'cities.id')
+            ->join('subareas', 'users.subarea_id', '=', 'subareas.id')
+            ->join('titles', 'users.title_id', '=', 'titles.id')
+            ->join('organizations', 'users.organization_id', '=', 'organizations.id')
+            ->select('opportunities.id as opportunity_id', 
+                'users.first_name as user_name', 
+                'cities.title as city_name',
+                'subareas.name as subarea_name',
+                'titles.name as title_name',
+                'organizations.name as organization_name')
+            ->first();
+        ;
 
-        if ($contact == null) {
+        if ($opportunity == null) {
             return response(status: Response::HTTP_NOT_FOUND);
         }
 
-        return response()->json($contact);
+        return response()->json($opportunity);
     }
 }
