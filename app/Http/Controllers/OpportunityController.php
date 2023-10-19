fc4db8fc2ebcb0097554b955a8ad8e747b05fcc4
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-10-19 00:24:20 -0300

    feat: add informações para o retorno do endpoit de oportunidades

--- app/Http/Controllers/OpportunityController.php
+++ app/Http/Controllers/OpportunityController.php
@@ -14,19 +14,22 @@ class OpportunityController extends Controller
     public function index()
     {
         $user_id = auth()->id();  
-           
+        
         $opportunities = Opportunity::where('user_id', $user_id)
             ->join('users', 'opportunities.interesting_user_id', '=', 'users.id')
             ->join('cities', 'users.city_id', '=', 'cities.id')
+            ->join('areas', 'users.area_id', '=', 'areas.id')
             ->join('subareas', 'users.subarea_id', '=', 'subareas.id')
             ->join('titles', 'users.title_id', '=', 'titles.id')
             ->join('organizations', 'users.organization_id', '=', 'organizations.id')
             ->select('opportunities.id as opportunity_id', 
-                'users.first_name as user_name', 
+                'users.first_name as user_name',
                 'cities.title as city_name',
+                'areas.name as area_name',
                 'subareas.name as subarea_name',
                 'titles.name as title_name',
-                'organizations.name as organization_name')
+                'organizations.name as organization_name',
+                'organizations.acronym as contact_organization_acronym')
             ->orderBy('opportunities.created_at', 'desc')
             ->get();
 
@@ -41,17 +44,19 @@ class OpportunityController extends Controller
         $opportunity = Opportunity::where('opportunities.id', $id)
             ->join('users', 'opportunities.interesting_user_id', '=', 'users.id')
             ->join('cities', 'users.city_id', '=', 'cities.id')
+            ->join('areas', 'users.area_id', '=', 'areas.id')
             ->join('subareas', 'users.subarea_id', '=', 'subareas.id')
             ->join('titles', 'users.title_id', '=', 'titles.id')
             ->join('organizations', 'users.organization_id', '=', 'organizations.id')
             ->select('opportunities.id as opportunity_id', 
-                'users.first_name as user_name', 
+                'users.first_name as user_name',
                 'cities.title as city_name',
+                'areas.name as area_name',
                 'subareas.name as subarea_name',
                 'titles.name as title_name',
-                'organizations.name as organization_name')
+                'organizations.name as organization_name',
+                'organizations.acronym as contact_organization_acronym')
             ->first();
-        ;
 
         if ($opportunity == null) {
             return response(status: Response::HTTP_NOT_FOUND);
